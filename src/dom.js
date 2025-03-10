7e79e476f9f7aac7f319cbac31b84691eb65bb61
A6ih <177154114+A6ih@users.noreply.github.com>
2025-03-10 23:24:41 +0200

    add conditionals to check activeUnit in renderTemps function

--- src/dom.js
+++ src/dom.js
@@ -2,20 +2,25 @@ const location = document.querySelector("#location-input");
 const tempDisplay = document.querySelector("#temp-display");
 const feelslikeDisplay = document.querySelector("#feelslike-display");
 const cityDisplay = document.querySelector("#city-display");
-let activeUnit = 'celsius';
+let activeUnit = "celsius";
 
 export function getLocation() {
-    console.log(location.value)
-    return location.value
+  console.log(location.value);
+  return location.value;
 }
 
 export function renderTemp(temp, feelslike) {
-    tempDisplay.textContent = temp;
-    feelslikeDisplay.textContent = "Feels like " + feelslike
+  if ((activeUnit = "celsius")) {
+    tempDisplay.textContent = (((temp - 32) * 5) / 9).toFixed(1);
+    feelslikeDisplay.textContent =
+      "Feels like " + (((feelslike - 32) * 5) / 9).toFixed(1) + "°C";
+  } else {
+    tempDisplay.textContent = temp.toFixed(1);
+    feelslikeDisplay.textContent = "Feels like " + feelslike.toFixed(1) + "°C";
+  }
 }
 
 export function renderWeather(temp, feelslike, city) {
-    renderTemp(temp, feelslike);
-    cityDisplay.textContent = city;
+  renderTemp(temp, feelslike);
+  cityDisplay.textContent = city;
 }
-
