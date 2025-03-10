3fdd6e87943a1568de4280e1315e5a9bf991425e
A6ih <177154114+A6ih@users.noreply.github.com>
2025-03-10 15:51:30 +0200

    Add airbnb style base and prettier packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,23 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname, // Optional, default to process.cwd()
+});
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  ...compat.extends('eslint-config-airbnb-base'),
+  ...compat.plugins("airbnb"),
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
